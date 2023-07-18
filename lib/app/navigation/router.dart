72e38e66e6170393615022693202819ca040af27
vladcto <55505918+vladcto@users.noreply.github.com>
2023-07-18 23:35:55 +0300

    refactor

--- lib/app/navigation/router.dart
+++ lib/app/navigation/router.dart
@@ -1,14 +1,29 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cocktail_gen/app/navigation/navigation_url.dart';
 import 'router.gr.dart';
 
 @AutoRouterConfig(replaceInRouteName: 'Page,Route')
 class AppRouter extends $AppRouter {
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(page: MainRoute.page, initial: true, children: [
-          AutoRoute(page: CocktailsRoute.page),
-          AutoRoute(page: GeneratorRoute.page),
-          AutoRoute(page: IngredientsRoute.page),
-        ]),
+        AutoRoute(
+          page: MainRoute.page,
+          initial: true,
+          children: [
+            AutoRoute(
+              page: CocktailsRoute.page,
+              path: NavigationUrl.cocktail,
+            ),
+            AutoRoute(
+              page: GeneratorRoute.page,
+              path: NavigationUrl.generate,
+            ),
+            AutoRoute(
+              page: IngredientsRoute.page,
+              path: NavigationUrl.ingredients,
+            ),
+          ],
+        ),
+        RedirectRoute(path: "*", redirectTo: NavigationUrl.cocktail),
       ];
 }
