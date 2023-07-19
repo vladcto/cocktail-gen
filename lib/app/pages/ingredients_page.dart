4e9f5af7ee087dc0888d0cacfefa379ed39786e5
vladcto <55505918+vladcto@users.noreply.github.com>
2023-07-19 21:02:18 +0300

    dsgn: Отступы и тема текста карточки ингредиента.

--- lib/app/pages/ingredients_page.dart
+++ lib/app/pages/ingredients_page.dart
@@ -1,20 +1,31 @@
 import 'package:auto_route/annotations.dart';
+import 'package:cocktail_gen/app/constants/app_paddings.dart';
 import 'package:cocktail_gen/app/widgets/ingredient_tile.dart';
 import 'package:cocktail_gen/data/repos/mock_ingredient.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
 class IngredientsPage extends StatelessWidget {
+  static const double itemsPadding = AppPaddings.medium;
+
   const IngredientsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView(
+      padding: const EdgeInsets.symmetric(
+        // Ставим отступ в [large] учитывая отступ вокруг детей.
+        vertical: AppPaddings.large - itemsPadding / 2,
+        horizontal: AppPaddings.medium,
+      ),
       children: [
         for (final ingredient in MockIngredient.data)
-          SizedBox(
-            height: 96,
-            child: IngredientTile(ingredient: ingredient),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: itemsPadding / 2),
+            child: SizedBox(
+              height: 96,
+              child: IngredientTile(ingredient: ingredient),
+            ),
           ),
       ],
     );
--- lib/app/widgets/ingredient_tile.dart
+++ lib/app/widgets/ingredient_tile.dart
@@ -1,3 +1,5 @@
+import 'package:cocktail_gen/app/constants/app_font_size.dart';
+import 'package:cocktail_gen/app/constants/app_paddings.dart';
 import 'package:cocktail_gen/app/constants/app_radius.dart';
 import 'package:cocktail_gen/domain/entities/ingredient.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +11,8 @@ class IngredientTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
     return Row(
       children: [
         AspectRatio(
@@ -18,8 +22,15 @@ class IngredientTile extends StatelessWidget {
             child: Image.network(ingredient.imageUrl),
           ),
         ),
+        const SizedBox(width: AppPaddings.small),
         Expanded(
-          child: Text(ingredient.name),
+          child: Text(
+            ingredient.name,
+            style: TextStyle(
+              fontSize: AppFontSize.title,
+              color: colorScheme.onSurface,
+            ),
+          ),
         ),
       ],
     );
