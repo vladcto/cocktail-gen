a76d98b461873386feedb81e2abda2ce024ae121
vladcto <55505918+vladcto@users.noreply.github.com>
2023-07-20 21:35:36 +0300

    feat: Набросал страницы шагов и описания.

--- lib/app/pages/cocktail_preview_page.dart
+++ lib/app/pages/cocktail_preview_page.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cocktail_gen/app/widgets/additional_layout_info.dart';
+import 'package:cocktail_gen/app/widgets/cocktail_preview/steps_preview.dart';
 import 'package:cocktail_gen/app/widgets/theme_segmented_button.dart';
 import 'package:cocktail_gen/data/repos/mock_cocktail.dart';
 import 'package:cocktail_gen/domain/entities/cocktail.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 enum PreviewType {
   description,
@@ -11,8 +13,12 @@ enum PreviewType {
   steps,
 }
 
+final _previewTypeProvider = StateProvider(
+  (ref) => PreviewType.description,
+);
+
 @RoutePage()
-class CocktailPreviewPage extends StatelessWidget {
+class CocktailPreviewPage extends ConsumerWidget {
   final Cocktail cocktail = MockCocktail.cocktail;
 
   CocktailPreviewPage({
@@ -20,7 +26,7 @@ class CocktailPreviewPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return AdditionalLayoutInfo(
       appBarText: cocktail.name,
       imageUrl: cocktail.imageUrl,
@@ -44,12 +50,38 @@ class CocktailPreviewPage extends StatelessWidget {
                   label: Text("Шаги"),
                 ),
               ],
-              initial: PreviewType.description,
-              onSelected: (_) {},
+              initial: ref.read(_previewTypeProvider),
+              onSelected: (type) {
+                ref.read(_previewTypeProvider.notifier).state = type;
+              },
             ),
           ),
+          switch (ref.watch(_previewTypeProvider)) {
+            PreviewType.description => _DescriptionPreview(cocktail: cocktail),
+            PreviewType.steps => StepsPreview(steps: cocktail.steps),
+            _ => const SizedBox.shrink(),
+          }
         ],
       ),
     );
+  }
+}
+
+class _DescriptionPreview extends StatelessWidget {
+  final Cocktail cocktail;
+  const _DescriptionPreview({required this.cocktail});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(cocktail.description);
+  }
+}
+
+class _IngredientPreview extends StatelessWidget {
+  const _IngredientPreview();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
