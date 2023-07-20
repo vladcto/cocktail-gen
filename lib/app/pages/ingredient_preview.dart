7adc1bff56df53502174bfefc8a8b984fddf6c1f
vladcto <55505918+vladcto@users.noreply.github.com>
2023-07-20 06:05:43 +0300

    dsgn: набросал макет экрана просмотра ингредиента.

--- lib/app/pages/ingredient_preview.dart
+++ lib/app/pages/ingredient_preview.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:auto_route/auto_route.dart';
+import 'package:cocktail_gen/app/constants/app_font_size.dart';
 import 'package:cocktail_gen/app/widgets/url_back_button.dart';
 import 'package:cocktail_gen/data/repos/mock_ingredient.dart';
+import 'package:cocktail_gen/domain/entities/ingredient.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -10,37 +14,76 @@ class IngredientPreviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ingredient = MockIngredient.ingredient;
-    final width = MediaQuery.of(context).size.width;
 
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: width,
-            automaticallyImplyLeading: true,
-            leading: const UrlBackButton(),
-            flexibleSpace: FlexibleSpaceBar(
-              background: Image.network(
-                ingredient.imageUrl,
-                fit: BoxFit.cover,
-              ),
-              title: Text(
-                ingredient.name,
-                style: const TextStyle(
-                  color: Colors.red,
-                ),
-              ),
+          ThemeSliverAppBar(ingredient: ingredient),
+          SliverToBoxAdapter(
+            child: Text(
+              ingredient.description,
+              style: const TextStyle(fontSize: 32),
             ),
           ),
-          SliverToBoxAdapter(
-            child: GestureDetector(
-              child: const SizedBox(
-                height: 10000,
-              ),
-            ),
-          )
         ],
+      ),
+    );
+  }
+}
+
+class ThemeSliverAppBar extends StatelessWidget {
+  const ThemeSliverAppBar({
+    super.key,
+    required this.ingredient,
+  });
+
+  final Ingredient ingredient;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return SliverAppBar(
+      pinned: true,
+      expandedHeight: width,
+      automaticallyImplyLeading: true,
+      backgroundColor: Colors.transparent,
+      leading: const UrlBackButton(),
+      flexibleSpace: FlexibleSpaceBar(
+        background: Image.network(
+          ingredient.imageUrl,
+          fit: BoxFit.cover,
+        ),
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        expandedTitleScale: 1,
+        title: LayoutBuilder(
+          builder: (context, constraints) {
+            double perc = min(
+              1,
+              (constraints.maxHeight - kToolbarHeight) / (width * 0.7),
+            );
+
+            return Container(
+              width: double.infinity,
+              height: kToolbarHeight + 32 * perc,
+              color: colorScheme.background.withOpacity(
+                max(0.6, 1 - perc),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                ingredient.name,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: AppFontSize.title,
+                  color: colorScheme.secondary,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/data/repos/mock_ingredient.dart
+++ lib/data/repos/mock_ingredient.dart
@@ -4,7 +4,11 @@ class MockIngredient {
   static final Ingredient ingredient = Ingredient(
     id: 1,
     name: "Самбука",
-    description: "Длинное описание",
+    description: """Итальянский и испанский традиционный напиток — ликёр с ароматом аниса. 
+
+Обычно это прозрачная сладкая вязкая жидкость со специфическим ароматом и содержанием спирта 38—42 %.
+
+В то же время имеются тёмные и даже красные разновидности самбуки.""",
     imageUrl:
         "https://irecommend.ru/sites/default/files/product-images/290233/O1DWqpsEiz63ZIJY2GFd1A.jpg",
   );
