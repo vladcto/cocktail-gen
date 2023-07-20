18a80a1dd03b7944730b6bf620f2f98b5b2d5e93
vladcto <55505918+vladcto@users.noreply.github.com>
2023-07-20 16:18:23 +0300

    refactor

--- lib/app/pages/ingredient_preview.dart
+++ lib/app/pages/ingredient_preview.dart
@@ -1,17 +1,16 @@
-import 'dart:math';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:cocktail_gen/app/constants/app_font_size.dart';
 import 'package:cocktail_gen/app/constants/app_paddings.dart';
 import 'package:cocktail_gen/app/constants/app_radius.dart';
 import 'package:cocktail_gen/app/widgets/url_back_button.dart';
 import 'package:cocktail_gen/data/repos/mock_ingredient.dart';
-import 'package:cocktail_gen/domain/entities/ingredient.dart';
 import 'package:flutter/material.dart';
 
+/// Страница, отображающая информация о ингредиенте.
 @RoutePage()
 class IngredientPreviewPage extends StatelessWidget {
   static const double imagePadding = 32;
+  static const fontWeight = FontWeight.w500;
   const IngredientPreviewPage({Key? key}) : super(key: key);
 
   @override
@@ -29,7 +28,7 @@ class IngredientPreviewPage extends StatelessWidget {
           style: TextStyle(
             fontSize: AppFontSize.title,
             color: colorScheme.secondary,
-            fontWeight: FontWeight.w500,
+            fontWeight: fontWeight,
           ),
         ),
       ),
@@ -46,6 +45,7 @@ class IngredientPreviewPage extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
+          // Виджет текста и подложки
           Positioned.fill(
             child: ListView(
               children: [
@@ -70,7 +70,7 @@ class IngredientPreviewPage extends StatelessWidget {
                         ingredient.type,
                         style: TextStyle(
                           fontSize: AppFontSize.title,
-                          fontWeight: FontWeight.w500,
+                          fontWeight: fontWeight,
                           color: colorScheme.primary,
                         ),
                       ),
@@ -79,7 +79,7 @@ class IngredientPreviewPage extends StatelessWidget {
                         ingredient.description,
                         style: TextStyle(
                           fontSize: AppFontSize.title,
-                          fontWeight: FontWeight.w500,
+                          fontWeight: fontWeight,
                           color: colorScheme.onSurface,
                         ),
                       ),
@@ -90,64 +90,6 @@ class IngredientPreviewPage extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class ThemeSliverAppBar extends StatelessWidget {
-  const ThemeSliverAppBar({
-    super.key,
-    required this.ingredient,
-  });
-
-  final Ingredient ingredient;
-
-  @override
-  Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    final colorScheme = Theme.of(context).colorScheme;
-
-    return SliverAppBar(
-      pinned: true,
-      expandedHeight: width,
-      automaticallyImplyLeading: true,
-      backgroundColor: Colors.transparent,
-      leading: const UrlBackButton(),
-      flexibleSpace: FlexibleSpaceBar(
-        background: Image.network(
-          ingredient.imageUrl,
-          fit: BoxFit.cover,
-        ),
-        centerTitle: true,
-        titlePadding: const EdgeInsets.all(0),
-        expandedTitleScale: 1,
-        title: LayoutBuilder(
-          builder: (context, constraints) {
-            double perc = min(
-              1,
-              (constraints.maxHeight - kToolbarHeight) / (width * 0.7),
-            );
-
-            return Container(
-              width: double.infinity,
-              height: kToolbarHeight + 32 * perc,
-              color: colorScheme.background.withOpacity(
-                max(0.6, 1 - perc),
-              ),
-              alignment: Alignment.center,
-              child: Text(
-                ingredient.name,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: AppFontSize.title,
-                  color: colorScheme.secondary,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            );
-          },
-        ),
       ),
     );
   }
