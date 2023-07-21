23bafe065a18e446f44f729f768505d23a6f5ccd
vladcto <55505918+vladcto@users.noreply.github.com>
2023-07-21 17:26:05 +0300

    feat: Простая разметка страницы генерации.

--- lib/app/pages/generator_page.dart
+++ lib/app/pages/generator_page.dart
@@ -5,6 +5,9 @@ import 'package:cocktail_gen/app/pages/generated_cocktail_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final _tagsProvider = StateProvider<String>((_) => "");
+final _ingredientsProvider = StateProvider<String>((_) => "");
+
 @RoutePage()
 class GeneratorPage extends ConsumerWidget {
   const GeneratorPage({Key? key}) : super(key: key);
@@ -14,21 +17,40 @@ class GeneratorPage extends ConsumerWidget {
     final router = context.router;
 
     return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          ref
-              .watch(gptClientProvider)
-              .generateRecipe(
-                "шипящий",
-                "джин",
-              )
-              .then(
-                (e) => router.pushWidget(
-                  GeneratedCocktailPreview(cocktail: e),
-                ),
-              );
-        },
-        child: Text("Press me"),
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              label: Text("Теги, через запятую"),
+            ),
+            onChanged: (val) => ref.read(_tagsProvider.notifier).state = val,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              label: Text("Ингредиенты, через запятую"),
+            ),
+            onChanged: (val) =>
+                ref.read(_ingredientsProvider.notifier).state = val,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final tags = ref.read(_tagsProvider);
+              final ingredients = ref.read(_ingredientsProvider);
+              ref
+                  .read(gptClientProvider)
+                  .generateRecipe(
+                    tags,
+                    ingredients,
+                  )
+                  .then(
+                    (e) => router.pushWidget(
+                      GeneratedCocktailPreview(cocktail: e),
+                    ),
+                  );
+            },
+            child: Text("Press me"),
+          ),
+        ],
       ),
     );
   }
